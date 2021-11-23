3f00ef2148bbee796d972da16c9211c86acc7cbc
Macéo <maceo.jalbert@epitech.eu>
2021-11-23 14:30:12 +0100

    :feat: add current lib

---
+++ lib/my/my_str_isnum.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** my_str_isnum
+** File description:
+** fonction for my library
+*/
+
+#include "../headers/my.h"
+
+int my_str_isnum(char const *str)
+{
+    return 0;
+}
